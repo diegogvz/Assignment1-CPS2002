1e8d0159e4ece379f5cedb5c9b73b3a273139bd3
calvam10 <91256859+calvam10@users.noreply.github.com>
2024-02-02 17:35:26 +0100

    Creation Customer Class and modifications in its test

---
+++ Customer.java
@@ -0,0 +1,71 @@
+package src;
+
+import java.util.Random;
+
+public class Customer {
+
+    protected String name;
+    protected String surname;
+    protected int age;
+    protected String country;
+    protected String id;
+
+    public Customer(String name, String surname,String country,int age){
+        this.name = name;
+        this.surname = surname;
+        this.country = country;
+        this.age = age;
+        this.id = generateID();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getSurname(){
+        return this.surname;
+    }
+
+    public String getCountry(){
+        return this.country;
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+
+    public void setCountry(String country){
+        this.country = country;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String generateID() {
+        String characters = "0123456789";
+        StringBuilder id = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 8; ++i) {
+            char x = characters.charAt(random.nextInt(characters.length()));
+            id.append(x);
+        }
+        return id.toString();
+    }
+
+    public String displayInfo(){
+        return ("Name: " + this.getName() + ", Surname: " + this.getSurname() + ", Country: " + this.country + ", Age: " + this.getAge() + ", Id: " + this.getId());
+    }
+}
